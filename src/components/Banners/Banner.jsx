b44c2bb31d6c3c17905f93271036d005eca11bc7
May Khine Soe <maykhinesoeyl@gmail.com>
2025-03-31 14:32:41 +0630

    updated content

--- src/components/Banners/Banner.jsx
+++ src/components/Banners/Banner.jsx
@@ -1,35 +1,44 @@
-import React from 'react'
-import BannerPng from '../../assets/banner.jpg'
-import detail1 from '../../assets/detail1.jpg'
-import { motion } from 'framer-motion'
-import { FadeLeft } from '../../utility/Animation'
-import { IoBagHandleOutline } from 'react-icons/io5'
+import React from "react";
+import BannerPng from "../../assets/banner.jpg";
+import { motion } from "framer-motion";
 
 const bgStyle = {
-    backgroundImage: `url(${BannerPng})`,
-    backgroundPosition: "center",
-    backgroudSize: "cover",
-    backgroundRepeat: "no-repeat",
-    backgroundAttachment: "fixed",
-}
+  backgroundImage: `url(${BannerPng})`,
+  backgroundPosition: "center",
+  backgroundSize: "cover", // ✅ Fixed typo
+  backgroundRepeat: "no-repeat",
+  backgroundAttachment: "fixed",
+};
 
 const Banner = () => {
-    return (
-        <div className='container'>
-            <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-2">
-                <div>
-                    <img src={detail1} alt="" />
-                </div>
-                <div>
-                    <h1 className='text-3xl md:text-4xl pb-10 font-bold font-averia'>English Classes</h1>
-                    <p className='text-gray-500'>Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                        Ratione voluptas dolores repellendus nostrum placeat consectetur
-                        enim numquam dicta et, aperiam, tempore ut obcaecati fugiat consequatur
-                        veritatis ab ipsa illum tempora?</p>
-                </div>
+  return (
+    <section className="container mb-12 mt-24">
+      <div
+        style={bgStyle}
+        className="container grid grid-cols-1 md:grid-cols-2 space-y-6 md:space-y-0 py-14 rounded-3xl"
+      >
+        <div></div>
+        <div className="flex flex-col justify-center">
+          <motion.div
+            className="text-center md:text-left space-y-4 lg:max-w-[400px]"
+            initial={{ opacity: 0, x: 50 }}
+            animate={{ opacity: 1, x: 0 }}
+            transition={{ duration: 0.5 }}
+          >
+            <h1 className="text-3xl lg:text-6xl font-bold uppercase">Brand Info</h1>
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Optio reprehenderit in
+              pariatur, distinctio aut quae consequuntur beatae quas, laboriosam magni earum nobis
+              suscipit, debitis quisquam alias assumenda aperiam obcaecati culpa?
+            </p>
+            <div className="flex justify-center md:justify-start">
+              <button className="primary-btn">Learn More</button>
             </div>
+          </motion.div>
         </div>
-    )
-}
+      </div>
+    </section>
+  );
+};
 
-export default Banner
+export default Banner;
